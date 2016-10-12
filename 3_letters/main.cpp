fe17d593a9d3cb1c744713a74e9ae43d3f908230
fearsomepandacz <email.for.registers@gmail.com>
2016-10-12 02:48:54 +0200

    4

---
+++ 3_letters/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int factorial(int a) {
+    int x = 1;
+    for(a; a > 0; a--) {
+        x = x*a;
+    }
+    return x;
+}
+
+int combination(int a, int b) {
+    return factorial(a)/(factorial(a-b) * factorial(b));
+}
+
+int main(int argc, char **argv) {
+    const char *chars[4] = {"T","G","C","A"};
+    int x = combination(4, 3);
+    for(int i = 0; i < x; i++) {
+        printf("%c%c%c",chars[])
+    }
+}
---
+++ months/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+char month(int a) {
+    const char *mesic[9] = {"Leden","Unor","Brezen","Duben","Kveten","Cerven","Cervenec","Srpen","Zari"};
+    printf("Mesic je %s",mesic[a]);
+}
+
+int main(int argc, char **argv) {
+    int x;
+    printf("Cislo mesice: ");
+    scanf("%i", &x);
+    month(x);
+    return 0;
+}
