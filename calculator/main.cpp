0e2f1d5cfa3490cbe9b7e97d87f39dba3a534b1b
fearsomepandacz <email.for.registers@gmail.com>
2016-10-26 11:18:21 +0200

    1

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -2,14 +2,15 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <conio.h>
 
 int main(int argc, char **argv) {
     char a;
-    int b;
+    double b;
     char operation = NULL;
     bool bEmpty = true;
     while(1) {
-        a = getchar();
+        a = getch();
         if (a == 'Q') {
             break;
         } else {
@@ -20,26 +21,38 @@ int main(int argc, char **argv) {
                 bEmpty = false;
             } else if ((a == '+' || a == '-' || a == '/' || a == '*') && !bEmpty) {
                 operation = a;
-            } else if (operation == '+') {
-                printf("%d\n", b = b + (a - 48));
+            } else if (operation == '+' && (a < 58 && a > 47)) {
+                printf("%g\n", b = b + (a - 48));
                 operation = NULL;
-            } else if (operation == '-') {
-                printf("%d\n", b = b - (a - 48));
+            } else if (operation == '-' && (a < 58 && a > 47)) {
+                printf("%g\n", b = b - (a - 48));
                 operation = NULL;
-            } else if (operation == '*') {
-                printf("%d\n", b = b * (a - 48));
+            } else if (operation == '*' && (a < 58 && a > 47)) {
+                printf("%g\n", b = b * (a - 48));
                 operation = NULL;
-            } else if (operation == '/') {
-                printf("%d\n", b = b / (a - 48));
-                operation = NULL;
+            } else if (operation == '/' && (a < 58 && a > 47)) {
+                if((a - 48) != 0) {
+                    printf("%g\n", b = b / (a - 48));
+                    operation = NULL;
+                } else {
+                    operation = NULL;
+                    b = 0;
+                    printf("error\n");
+                }
             } else if (a == 'r' && !bEmpty) {
-                printf("%d\n", b = sqrt(b));
+                if(b > 0) {
+                    printf("%g\n", b = sqrt(b));
+                } else {
+                    b = 0;
+                    operation = NULL;
+                    printf("error\n");
+                }
             } else if (a == 's' && !bEmpty) {
-                printf("%d\n", b = sin(b));
+                printf("%g\n", b = sin(b));
             } else if (a == 'c' && !bEmpty) {
-                printf("%d\n", b = cos(b));
+                printf("%g\n", b = cos(b));
             } else if (a == 't' && !bEmpty) {
-                printf("%d\n", b = tan(b));
+                printf("%g\n", b = tan(b));
             }
         }
     }
